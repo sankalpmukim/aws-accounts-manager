8c5b13f631f0e67cd58eddfc7b12c90ae1667cb3
Sankalp Mukim <sankalpmukim@gmail.com>
2024-04-28 14:47:49 +0530

    Refactor popup component in popup.tsx

--- src/popup.tsx
+++ src/popup.tsx
@@ -10,6 +10,11 @@ import { useWhichPage } from "~hooks/useWhichPage";
 
 import "~style.css";
 
+import {
+  ManageAccountsModalProvider,
+  useOpenManageAccountsModal,
+} from "./context/ManageAccountModal";
+
 const SHOW_STATUS = {
   unknown: "Currently not on AWS signin page.",
   "aws-signin-initial": "Currently on AWS signin page.",
@@ -19,33 +24,40 @@ const SHOW_STATUS = {
 
 function Popup() {
   const openAddAccountModal = useOpenCreateAccountModal();
+  const openManageAccountsModal = useOpenManageAccountsModal();
   const [page, loading] = useWhichPage();
 
   return (
-    <div className="m-3 rounded-lg border-2 px-3">
-      <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
-        <div className="-ml-4 -mt-4 flex flex-wrap items-center justify-between sm:flex-nowrap">
-          <div className="ml-4 mt-4">
-            <h3 className="text-base font-semibold leading-6 text-gray-900">
-              {`AWS Accounts Manager`}
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">
-              {`${SHOW_STATUS[page] ?? SHOW_STATUS["unknown"]}${
-                loading ? ", Loading..." : ""
-              }`}
-            </p>
-          </div>
-          <div className="ml-4 mt-4 flex-shrink-0">
-            <button
-              type="button"
-              className="relative inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={openAddAccountModal}
-            >
-              {`Add account`}
-            </button>
-          </div>
+    <div className="m-3 rounded-lg border border-gray-200 shadow">
+      <div className="flex flex-col items-start justify-between bg-white p-4 md:flex-row md:items-center md:p-6">
+        <div>
+          <h3 className="text-lg font-semibold text-gray-900 md:text-xl">
+            AWS Accounts Manager
+          </h3>
+          <p className="mt-1 text-sm text-gray-500">
+            {`${SHOW_STATUS[page] ?? SHOW_STATUS["unknown"]}${
+              loading ? ", Loading..." : ""
+            }`}
+          </p>
+        </div>
+        <div className="mt-4 flex gap-4 md:mt-0">
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600 focus-visible:ring-offset-2"
+            onClick={openAddAccountModal}
+          >
+            Add Account
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-md bg-gray-50 px-4 py-2 text-sm font-medium text-gray-900 ring-1 ring-gray-300 hover:bg-gray-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600 focus-visible:ring-offset-2"
+            onClick={openManageAccountsModal}
+          >
+            Manage Accounts
+          </button>
         </div>
       </div>
+      <hr className="border-t border-gray-200" />
       <List />
     </div>
   );
@@ -55,9 +67,11 @@ function IndexPopup() {
   return (
     <CreateAccountModalProvider>
       <EditAccountModalProvider>
-        <Popup />
-        <CreateAccount />
-        <EditAccount />
+        <ManageAccountsModalProvider>
+          <Popup />
+          <CreateAccount />
+          <EditAccount />
+        </ManageAccountsModalProvider>
       </EditAccountModalProvider>
     </CreateAccountModalProvider>
   );
