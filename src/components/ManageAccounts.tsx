f096bd4118330400906300b37d6c68946404ea48
Sankalp Mukim <sankalpmukim@gmail.com>
2024-04-28 16:52:20 +0530

    Fix bug in ManageAccounts component and update import functionality

--- src/components/ManageAccounts.tsx
+++ src/components/ManageAccounts.tsx
@@ -119,6 +119,7 @@ export default function ManageAccounts() {
                             ...oldAccounts,
                             ...newAccounts,
                           ]);
+                          setModalIsOpen(false);
                         };
                         reader.readAsText(file);
                       }}
@@ -127,7 +128,10 @@ export default function ManageAccounts() {
                   <button
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                    onClick={() => console.log(`Clear all accounts`)}
+                    onClick={() => {
+                      setAccounts([]);
+                      setModalIsOpen(false);
+                    }}
                   >
                     {`Clear all accounts`}
                   </button>
